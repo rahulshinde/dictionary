b55cbbd7d0eb70e8016d2c2dba76ac1f496327f7
Rahul Subhash Shinde <rshinde@mica.edu>
2016-01-25 15:46:34 -0500

    big update, searchabletable + image api, still need to check how this works live (not just hosted locally)

--- js/app.js
+++ js/app.js
@@ -2,16 +2,23 @@
 angular.module('sortApp', [])
 
 .controller('mainController', function($scope) {
-  $scope.sortType     = 'fish'; // set the default sort type
+  $scope.sortType     = 'name'; // set the default sort type
   $scope.sortReverse  = false;  // set the default sort order
-  $scope.searchFish   = '';     // set the default search/filter term
+  $scope.searchName   = '';     // set the default search/filter term
   
+  var searchTerm = $scope.searchName;
+
   // create the list of sushi rolls 
   $scope.sushi = [
-    { url: 'https://www.google.com/', name: 'Cali Roll', fish: 'Crab', tastiness: 2 },
-    { url: 'https://www.google.com/', name: 'Philly', fish: 'Tuna', tastiness: 4 },
-    { url: 'https://www.google.com/', name: 'Tiger', fish: 'Eel', tastiness: 7 },
-    { url: 'https://www.google.com/', name: 'Rainbow', fish: 'Variety', tastiness: 6 }
+    { url: 'http://rahulshinde.github.io/prototype-3/', name: 'Exercise 1', description: 'An exercise in contained three dimensional symbolic systems. Viewer is placed external to the virtual environment. Scale between objects is fixed but can fluctuate in relation to the user.', tags: 'Three.js, 3D, web-experiments' },
+    { url: '2.html', name: 'Exercise 2', description: 'The color red is very enjoyable.', tags: 'color' },
+    { url: '3.html', name: 'Exercise 3', description: 'The color green is nice but I wish it was a bit brighter.', tags: 'color' },
+    { url: 'http://rahulshinde.github.io/prototype-3/', name: 'Exercise 4', description: 'An exercise in contained three dimensional symbolic systems. Viewer is placed external to the virtual environment. Scale between objects is fixed but can fluctuate in relation to the user.', tags: 'Three.js, 3D, web-experiments' },
+    { url: 'http://rahulshinde.github.io/prototype-3/', name: 'Exercise 5', description: 'An exercise in contained three dimensional symbolic systems. Viewer is placed external to the virtual environment. Scale between objects is fixed but can fluctuate in relation to the user.', tags: 'Three.js, 3D, web-experiments' },
+    { url: '3.html', name: 'Exercise 6', description: 'The color green is nice but I wish it was a bit brighter.', tags: 'color' },
   ];
-  
+
+  //add images using api
+  console.log(searchTerm);
+
 });
--- js/main.js
+++ js/main.js
@@ -1,21 +1,46 @@
 var apiKey = '3u9xpvuptwc3v279n95ecv2s';
 
-$.ajax(
-    {
-        type:'GET',
-        url:"https://api.gettyimages.com/v3/search/images?fields=comp&orientations=Horizontal&phrase=friendship,3d",
-         beforeSend: function (request)
-            {
-                request.setRequestHeader("Api-Key", apiKey);
-            }})
-    .done(function(data){
-        console.log("Success with data")
-        console.log(data)
-        for(var i = 0;i<data.images.length;i++)
+var text = '';
+
+function getImages() {
+
+    if (text == '') {
+        $('#output img').remove();
+        $("#output").append("<img src='http://www.placecage.com/150/100'>");
+        $("#output").append("<img src='http://www.placecage.com/150/100'>");
+        $("#output").append("<img src='http://www.placecage.com/150/100'>");
+        $("#output").append("<img src='http://www.placecage.com/150/100'>");
+        $("#output").append("<img src='http://www.placecage.com/150/100'>");
+        $("#output").append("<img src='http://www.placecage.com/150/100'>");
+        return;
+    }
+
+    console.log(text);
+    $('#output img').remove();
+
+    $.ajax(
         {
-           $("#output").append("<img src='" + data.images[i].display_sizes[0].uri + "'/>");
-        }
-    })
-    .fail(function(data){
-        alert(JSON.stringify(data,2))
-    });
+            type:'GET',
+            url:"https://api.gettyimages.com/v3/search/images?fields=thumb&orientations=Horizontal&phrase=" + text + ",3d",
+             beforeSend: function (request)
+                {
+                    request.setRequestHeader("Api-Key", apiKey);
+                }})
+        .done(function(data){
+            console.log("Success with data")
+            console.log(data)
+            for(var i = 0;i<data.images.length;i++)
+            {
+               $("#output").append("<img src='" + data.images[i].display_sizes[0].uri + "'/>");
+            }
+        })
+        .fail(function(data){
+            alert(JSON.stringify(data,2))
+        });
+}
+
+function search() {
+    text = $('input:text').val();
+
+    getImages();
+}
